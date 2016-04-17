0ba4ba876e6e54b3f439d3ad3d06a433744e1264
Dennis <dennisborsh@gmail.com>
2016-04-17 16:59:17 +0300

    fix
    fix

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
                 exclude: [/app\/parse/, /app\/lib/, /jspm_packages/, /.tmp/, /build/, /gulp/, /node_modules/, /bower_components/, /bld/],
                 loader: ['babel-loader'],
                 query: {
-                    plugins: ['transform-decorators-legacy', 'transform-object-assign'],
+                    plugins: ['transform-decorators-legacy', 'transform-object-assign','babel-plugin-transform-es2015-literals'],
                     presets: ['es2015', 'stage-0']
                 }
             },
